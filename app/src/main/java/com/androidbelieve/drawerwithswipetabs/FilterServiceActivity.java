1b8cf6c50e4c20d5251d1ff631ddc3aa0da2a04f
rng123 <lalthullu@gmail.com>
2016-12-25 22:22:12 +0530

    gk

---
+++ app/src/main/java/com/androidbelieve/drawerwithswipetabs/FilterServiceActivity.java
@@ -0,0 +1,229 @@
+package com.androidbelieve.drawerwithswipetabs;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import io.apptik.widget.MultiSlider;
+
+public class FilterServiceActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ArrayList<Filter> subcat=new ArrayList<>(),rent=new ArrayList<>();
+    private RecyclerView recyclerView;
+    private FilterAdapter adapter;
+    private String cat;
+    private void fillAds(String cat)
+    {
+        if(cat.equals("Electronics & Applications"))
+        {
+            subcat.add(new Filter("Mobile Phone"));
+            subcat.add(new Filter("Tablet"));
+            subcat.add(new Filter("Accessories"));
+            subcat.add(new Filter("Computer & Laptop"));
+            subcat.add(new Filter("TV,Video-Audio"));
+            subcat.add(new Filter("Printer"));
+            subcat.add(new Filter("Computer Accessories"));
+            subcat.add(new Filter("Camera & Lenses"));
+            subcat.add(new Filter("Kitchen Appliance"));
+            subcat.add(new Filter("Speakers"));
+            subcat.add(new Filter("Projectors"));
+            subcat.add(new Filter("Others"));
+        }
+        if(cat.equals("Furniture")) {
+            subcat.add(new Filter("Sofa"));
+            subcat.add(new Filter("Dining"));
+            subcat.add(new Filter("Bed"));
+            subcat.add(new Filter("Wardrobe"));
+            subcat.add(new Filter("Home Décor & Garden"));
+            subcat.add(new Filter("Others"));
+        }
+        if(cat.equals("Books, Sports & Hobbies")) {
+            subcat.add(new Filter("Book"));
+            subcat.add(new Filter("Musical Instrument"));
+            subcat.add(new Filter("Sports Equipment"));
+            subcat.add(new Filter("Travel & Camping"));
+            subcat.add(new Filter("Gaming"));
+            subcat.add(new Filter("Party Equipment"));
+            subcat.add(new Filter("Others"));
+        }
+        if(cat.equals("Fashion")) {
+            subcat.add(new Filter("Men"));
+            subcat.add(new Filter("Women"));
+            subcat.add(new Filter("Kid"));
+        }
+        if(cat.equals("Real Estate")) {
+            subcat.add(new Filter("Residential"));
+            subcat.add(new Filter("Commercial"));
+            subcat.add(new Filter("Others"));
+        }
+        if(cat.equals("Tools & Equipments")){
+            subcat.add(new Filter("Power Tool"));
+            subcat.add(new Filter("Spanner"));
+            subcat.add(new Filter("Others"));
+        }
+        if(cat.equals("Cars")) {
+            subcat.add(new Filter("Cars"));
+            subcat.add(new Filter("Commercial vehicle"));
+            subcat.add(new Filter("Others"));
+        }
+        if(cat.equals("Bike")){
+            subcat.add(new Filter("Bike"));
+            subcat.add(new Filter("Scooter"));
+            subcat.add(new Filter("Bicycle"));
+            subcat.add(new Filter("Others"));
+        }
+
+        rent.add(new Filter("Days"));
+        rent.add(new Filter("Weeks"));
+        rent.add(new Filter("Months"));
+    }
+
+
+    private ListView listView;
+    String[] filters={"Sub-Categories","Starting Range"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        cat=getIntent().getStringExtra("CAT");
+
+        fillAds(cat);
+        adapter=new FilterAdapter(FilterServiceActivity.this,subcat,false);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        recyclerView=(RecyclerView)findViewById(R.id.list2);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        ArrayAdapter filterAdapter=new ArrayAdapter<String>(this,R.layout.filter_list_view,R.id.tv_filter,filters);
+        listView= (ListView) findViewById(R.id.list1);
+        listView.setAdapter(filterAdapter);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Filter By");
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_keyboard_backspace_black_24dp));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0)
+                {
+                    recyclerView.setVisibility(View.VISIBLE);
+                    adapter=new FilterAdapter(FilterServiceActivity.this,subcat,false);
+                    recyclerView.setAdapter(adapter);
+                    LinearLayout ll= (LinearLayout) findViewById(R.id.ll_slider);
+                    ll.setVisibility(View.GONE);
+                    adapter.notifyDataSetChanged();
+                }
+                else if(position==1)
+                {
+                    final TextView min1 = (TextView) findViewById(R.id.minValue1);
+                    final TextView max1 = (TextView) findViewById(R.id.maxValue1);
+                    MultiSlider multiSlider1 = (MultiSlider) findViewById(R.id.range_slider1);
+                    LinearLayout ll= (LinearLayout) findViewById(R.id.ll_slider);
+                    ll.setVisibility(View.VISIBLE);
+                    recyclerView.setVisibility(View.GONE);
+                    min1.setText(String.valueOf(multiSlider1.getThumb(0).getValue()));
+                    max1.setText(String.valueOf(multiSlider1.getThumb(1).getValue()));
+                    multiSlider1.setOnThumbValueChangeListener(new MultiSlider.OnThumbValueChangeListener() {
+                        @Override
+                        public void onValueChanged(MultiSlider multiSlider, MultiSlider.Thumb thumb, int thumbIndex, int value) {
+                            if (thumbIndex == 0) {
+                                min1.setText(String.valueOf(value));
+                            } else {
+                                max1.setText(String.valueOf(value));
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_filter, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void apply()
+    {
+        String subcats="'";
+        for(Filter x:subcat)
+        {
+            if(x.getSelected())
+                subcats+=x.getName()+"','";
+        }
+        String finalsubcatquery;
+        if(subcats.equals("'"))
+            finalsubcatquery="";
+        else
+            finalsubcatquery=" and subcat IN ("+subcats+"')";
+
+        String rent="";
+        for(Filter x:this.rent)
+        {
+            if(x.getSelected())
+            {
+                rent=x.getName();
+            }
+        }
+        if(!rent.equals(""))
+            rent="and crent LIKE '%rent%'";
+        /*
+        String rentrange="";
+        if(adapter.max!=5000)
+            rentrange=" and rent < "+Integer.toString(adapter.max);
+        String deposit="";
+        if(adapter.deposit!=6000)
+            deposit=" and PROD_DEPOSIT";
+        */
+
+        String finalfilter=finalsubcatquery+rent;
+        Intent intent=new Intent();
+        // intent.putStringArrayListExtra("im")
+        intent.putExtra("data",finalfilter);
+        setResult(Activity.RESULT_OK,intent);
+        finish();
+
+        Log.v("final subcats",finalfilter);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_apply:
+                apply();
+                return true;
+            case R.id.nav_clear:
+            {
+                subcat.clear();
+                rent.clear();
+                fillAds(cat);
+                adapter.notifyDataSetChanged();
+            }
+            return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
